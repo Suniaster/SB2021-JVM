9d0fe35c4a70874d0e571d07ff0b2fdb0e17f8ec
Gabriel <patonoideoriginal@gmail.com>
2021-03-06 19:22:01 -0300

    feature: adiciona float e integer e resolve problema

---
+++ src/constant_pool_classes/integer_info.cpp
@@ -0,0 +1,16 @@
+#include "../../include/constant_pool_classes/integer_info.hpp"
+
+
+
+CP::IntegerInfo::IntegerInfo(ClassFile* class_file) : CpInfo(class_file){
+  this->tag = 3;
+}
+
+void CP::IntegerInfo::setInfo(){
+  this->class_file->setAttribute<uint32_t>(4, this->bytes);
+}
+
+void CP::IntegerInfo::printInfo(){
+  cout << "Integer: NI " << this->bytes << endl;
+
+}
--- src/cp_info.cpp
+++ src/cp_info.cpp
@@ -6,6 +6,7 @@
 #include "../include/constant_pool_classes/name_and_type_info.hpp"
 #include "../include/constant_pool_classes/field_ref_info.hpp"
 #include "../include/constant_pool_classes/interface_method_ref_info.hpp"
+#include "../include/constant_pool_classes/integer_info.hpp"
 
 CpInfo::CpInfo(ClassFile* class_file){
   this->class_file = class_file;
@@ -17,6 +18,9 @@ CpInfo* CpInfo::getInstance(uint8_t tag, ClassFile* class_file){
   case 0x1:
     correct_instance = new CP::Utf8Info(class_file);
     break;
+  case 0x3:
+    correct_instance = new CP::IntegerInfo(class_file);
+    break;
   case 0x7:
     correct_instance = new CP::ClassInfo(class_file);
     break;
@@ -26,13 +30,13 @@ CpInfo* CpInfo::getInstance(uint8_t tag, ClassFile* class_file){
   case 0x9:
     correct_instance = new CP::FieldRefInfo(class_file);
     break;
-  case 0x10:
+  case 0xa:
     correct_instance = new CP::MethodRefInfo(class_file);
     break;
-  case 0x11:
+  case 0xb:
     correct_instance = new CP::InterfaceMethodRefInfo(class_file);
     break;
-  case 0x12:
+  case 0xc:
     correct_instance = new CP::NameAndTypeInfo(class_file);
     break;
 
