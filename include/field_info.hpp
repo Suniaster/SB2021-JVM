ca6dfb0fa71b2c658fc0f76dcebf590cfbcce36d
Lary15 <lari1415@hotmail.com>
2021-03-06 17:34:30 -0300

    Created field_info.hpp

---
+++ include/field_info.hpp
@@ -0,0 +1,25 @@
+#ifndef __FIELD_INFO_H__
+#define __FIELD_INFO_H__
+
+#include <vector>
+#include <string>
+#include "./class_file.hpp"
+#include "./attribute_info.hpp"
+
+using namespace std;
+
+class FieldInfo{
+private:
+    uint16_t access_flags;
+    uint16_t name_index;
+    uint16_t descriptor_index;
+    uint16_t attributes_count;
+    vector<AttributeInfo*> attributes;
+    ClassFile *class_file;
+
+public:
+    FieldInfo(ClassFile *class_file);
+};
+
+
+#endif
