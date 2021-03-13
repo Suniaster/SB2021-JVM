da7432df0713ff0ca2a2cb276bc7b6b95cf1fbc3
titofrota <italoeduardo.df@hotmail.com>
2021-03-13 17:52:38 -0300

    ADD method_info.hpp

---
+++ include/method_info.hpp
@@ -0,0 +1,23 @@
+#ifndef __METHOD_INFO_H__
+#define __METHOD_INFO_H__
+class MethodInfo;
+
+#include <vector>
+#include <string>
+#include "./class_file.hpp"
+#include "./attribute_info.hpp"
+
+class MethodInfo{
+  private:
+    uint16_t access_flags;
+    uint16_t name_index;
+    uint16_t descriptor_index;
+    uint16_t attributes_count;
+    vector<Attribute_info> * attributes;
+
+  public:
+    
+
+};
+
+#endif
