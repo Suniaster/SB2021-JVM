17faf1b306f6997437577bd7728dff35919f6547
Ítalo Frota <56638525+titofrota@users.noreply.github.com>
2021-03-18 21:42:30 -0300

    Update method_info.cpp

--- src/method_info.cpp
+++ src/method_info.cpp
@@ -6,7 +6,7 @@ MethodInfo::MethodInfo(ClassFile *class_file) {
     this->class_file->file_reader->readBytes(2, this->name_index);
     this->class_file->file_reader->readBytes(2, this->descriptor_index);
     this->class_file->file_reader->readBytes(2, this->attributes_count);
-    AttributeInfo::loadAttributes(this->attributes, 1, this->class_file);
+    AttributeInfo::loadAttributes(this->attributes_count, this->class_file);
 }
 
 void MethodInfo::printInfo() {
