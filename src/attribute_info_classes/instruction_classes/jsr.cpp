b8111039f55ae554c18cf7a6d58a16b286556fc3
clarice42 <claramaia820@gmail.com>
2021-04-18 21:59:08 -0300

    Add instruction jsr (not finished)

--- src/attribute_info_classes/instruction_classes/jsr.cpp
+++ src/attribute_info_classes/instruction_classes/jsr.cpp
@@ -11,3 +11,14 @@ string Jsr::toString(){
   return "jsr " + this->intToString(this->branch_bytes);
 }
 
+int Jsr::execute(Frame *frame) {
+  int16_t branch_offset = this->branch_bytes;
+  uint32_t base_adress = frame->local_pc;
+
+  // falta fazer um push na pilha de operandos do opcode da proxima
+  // instrução, é do tipo returnAddress
+
+  frame->local_pc = base_adress + branch_offset;
+  return frame->local_pc;
+}
+
