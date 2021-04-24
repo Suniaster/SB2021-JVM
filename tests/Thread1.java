833c799ba05f53bf7254b49572bc022742d1e87d
Gabriel <patonoideoriginal@gmail.com>
2021-04-24 18:42:40 -0300

    chore: adiciona teste com thread

---
+++ tests/Thread1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gabriel
+ */
+public class Thread1 extends Thread{
+    
+    public void run(){
+        System.out.println("Eu sou uma thread");
+    }
+    
+}
---
+++ tests/ThreadTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gabriel
+ */
+public class ThreadTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Thread1 thread1 = new Thread1();
+       System.out.println("Eu sou a main");
+       thread1.run();
+    }
+    
+}
