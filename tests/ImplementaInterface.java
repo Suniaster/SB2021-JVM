2f7770deba223c46e50bd61b43d69c1d4f395e1a
Gabriel <patonoideoriginal@gmail.com>
2021-04-24 21:36:05 -0300

    chore: adiciona teste de interface

---
+++ tests/ImplementaInterface.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minhasegundainterface;
+
+/**
+ *
+ * @author gabriel
+ */
+public class ImplementaInterface implements Interface{
+
+    @Override
+    public int funcaoX(int x) {
+        System.out.println(x);
+        return x;
+    }
+
+    @Override
+    public int funcaoY(int y) {
+        System.out.println(y);
+        return y;
+    }
+    
+}
